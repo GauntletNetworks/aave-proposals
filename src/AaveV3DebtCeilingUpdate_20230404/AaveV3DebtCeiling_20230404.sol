50976b724b4bc8412facd554156bd0851e3d507e
Jonathan Reem <jonathan.reem@gmail.com>
2023-04-03 16:01:39 -0700

    Add avalanche steward for the avalanche portion of the debt ceiling update.

--- src/AaveV3DebtCeilingUpdate_20230404/AaveV3DebtCeiling_20230404.sol
+++ src/AaveV3DebtCeilingUpdate_20230404/AaveV3DebtCeiling_20230404.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.16;
 import 'aave-helpers/v3-config-engine/AaveV3PayloadPolygon.sol';
 import 'aave-helpers/v3-config-engine/AaveV3PayloadArbitrum.sol';
+import 'aave-helpers/v3-config-engine/AaveV3PayloadAvalanche.sol';
+import {IACLManager} from 'aave-address-book/AaveV3.sol';
 
 /**
  * @dev Payload for updating the debt ceiling of EURS on Polygon
@@ -53,6 +55,39 @@ contract AaveV3ArbDebtCeiling_20230404 is AaveV3PayloadArbitrum {
       liqThreshold: EngineFlags.KEEP_CURRENT,
       liqBonus: EngineFlags.KEEP_CURRENT,
       debtCeiling: 25_000,
+      liqProtocolFee: EngineFlags.KEEP_CURRENT,
+      eModeCategory: EngineFlags.KEEP_CURRENT
+    });
+
+    return collateralsUpdate;
+  }
+}
+
+/**
+ * @dev Payload for updating the debt ceiling of EURS and USDT on Arbitrum using the Guardian
+ * @author Gauntlet
+ * - Snapshot: https://snapshot.org/#/aave.eth/proposal/0xb1f8b1814d9c09760daa4d7e9fa90d906b2c9ff5fbd20c8d0f5793c4f5cc4999
+ * - Discussion: https://governance.aave.com/t/gauntlet-aave-v3-isolation-mode-methodology/12290
+ */
+contract AaveV3AvaDebtCeiling_20230404 is AaveV3PayloadAvalanche {
+  function _postExecute() internal override {
+    IACLManager aclManager = AaveV3Avalanche.ACL_MANAGER;
+
+    aclManager.renounceRole(
+      aclManager.RISK_ADMIN_ROLE(),
+      address(this)
+    );
+  }
+
+  function collateralsUpdates() public pure override returns (IEngine.CollateralUpdate[] memory) {
+    IEngine.CollateralUpdate[] memory collateralsUpdate = new IEngine.CollateralUpdate[](1);
+
+    collateralsUpdate[0] = IEngine.CollateralUpdate({
+      asset: AaveV3AvalancheAssets.FRAX_UNDERLYING,
+      ltv: EngineFlags.KEEP_CURRENT,
+      liqThreshold: EngineFlags.KEEP_CURRENT,
+      liqBonus: EngineFlags.KEEP_CURRENT,
+      debtCeiling: 1_000_000,
       liqProtocolFee: EngineFlags.KEEP_CURRENT,
       eModeCategory: EngineFlags.KEEP_CURRENT
     });
--- src/AaveV3DebtCeilingUpdate_20230404/AaveV3DebtCeiling_20230404_Test.t.sol
+++ src/AaveV3DebtCeilingUpdate_20230404/AaveV3DebtCeiling_20230404_Test.t.sol
@@ -5,10 +5,12 @@ import 'forge-std/Test.sol';
 
 import {AaveV3Polygon, AaveV3PolygonAssets} from 'aave-address-book/AaveV3Polygon.sol';
 import {AaveV3Arbitrum, AaveV3ArbitrumAssets} from 'aave-address-book/AaveV3Arbitrum.sol';
-import {AaveV3PolDebtCeiling_20230404,AaveV3ArbDebtCeiling_20230404} from './AaveV3DebtCeiling_20230404.sol';
+import {AaveV3Avalanche, AaveV3AvalancheAssets} from 'aave-address-book/AaveV3Avalanche.sol';
+import {AaveV3PolDebtCeiling_20230404,AaveV3ArbDebtCeiling_20230404,AaveV3AvaDebtCeiling_20230404} from './AaveV3DebtCeiling_20230404.sol';
 import {TestWithExecutor} from 'aave-helpers/GovHelpers.sol';
 import {AaveGovernanceV2} from 'aave-address-book/AaveGovernanceV2.sol';
 import {ProtocolV3TestBase, ReserveConfig} from 'aave-helpers/ProtocolV3TestBase.sol';
+import {IACLManager} from 'aave-address-book/AaveV3.sol';
 
 contract AaveV3PolDebtCeiling_20230404_Test is ProtocolV3TestBase, TestWithExecutor {
   function setUp() public {
@@ -86,5 +88,50 @@ contract AaveV3ArbDebtCeiling_20230404_Test is ProtocolV3TestBase, TestWithExecu
     ProtocolV3TestBase._validateReserveConfig(usdtBefore, allConfigsAfter);
 
     diffReports('preAaveV3ArbDebtCeiling_20230404_Change', 'postAaveV3ArbDebtCeiling_20230404_Change');
+  }
+}
+
+contract AaveV3AvaDebtCeiling_20230404_Test is ProtocolV3TestBase {
+  address public constant GUARDIAN_AVALANCHE =
+      0xa35b76E4935449E33C56aB24b23fcd3246f13470;
+
+  function setUp() public {
+    vm.createSelectFork(vm.rpcUrl('avalanche'), 28282486);
+  }
+
+  function testPayload() public {
+    ReserveConfig[] memory allConfigsBefore = _getReservesConfigs(AaveV3Avalanche.POOL);
+
+    createConfigurationSnapshot('preAaveV3AvaDebtCeiling_20230404_Change', AaveV3Avalanche.POOL);
+
+    vm.startPrank(GUARDIAN_AVALANCHE);
+
+    AaveV3AvaDebtCeiling_20230404 proposalPayload = new AaveV3AvaDebtCeiling_20230404();
+    IACLManager aclManager = IACLManager(AaveV3Avalanche.ACL_MANAGER);
+    aclManager.grantRole(aclManager.RISK_ADMIN_ROLE(), address(proposalPayload));
+
+    vm.stopPrank();
+
+    proposalPayload.execute();
+
+    createConfigurationSnapshot('postAaveV3AvaDebtCeiling_20230404_Change', AaveV3Avalanche.POOL);
+
+    ReserveConfig[] memory allConfigsAfter = _getReservesConfigs(AaveV3Avalanche.POOL);
+
+    address[] memory assetsChanged = new address[](1);
+    assetsChanged[0] = AaveV3AvalancheAssets.FRAX_UNDERLYING;
+    _noReservesConfigsChangesApartFrom(allConfigsBefore, allConfigsAfter, assetsChanged);
+
+    ReserveConfig memory fraxBefore = ProtocolV3TestBase._findReserveConfig(
+      allConfigsBefore,
+      AaveV3AvalancheAssets.FRAX_UNDERLYING
+    );
+
+    fraxBefore.debtCeiling = 1_000_000_00;
+    ProtocolV3TestBase._validateReserveConfig(fraxBefore, allConfigsAfter);
+
+    diffReports('preAaveV3AvaDebtCeiling_20230404_Change', 'postAaveV3AvaDebtCeiling_20230404_Change');
+
+    assertFalse(aclManager.isRiskAdmin(address(proposalPayload)));
   }
 }
