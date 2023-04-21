e00b8cc828914099eb28970acf29fbc37e5ee349
Jonathan Reem <jonathan.reem@gmail.com>
2023-04-21 00:05:12 -0700

    Add wip generator tool.

---
+++ generator/deploy.ts
@@ -0,0 +1,43 @@
+import {AllUpdates, govHelperNetworkNames, Networks} from '.';
+
+export function generateDeployFile(updateDate: string, updates: AllUpdates): string {
+  return `// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {GovHelpers} from 'aave-helpers/GovHelpers.sol';
+import {
+${Object.keys(updates)
+  .map((network) => `  ${network}Script`)
+  .join(',\n')}
+} from 'aave-helpers/ScriptUtils.sol';
+import {
+${Object.keys(updates)
+  .map((network) => `  AaveV3${network}Update${updateDate}Payload`)
+  .join(',\n')}
+} from './AaveV3Update_${updateDate}.sol';
+
+contract CreateProposal is EthereumScript {
+  function run() external broadcast {
+    GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](${Object.keys(updates).length});
+${Object.keys(updates)
+  .map((network, index) => {
+    return `    payloads[${index}] = GovHelpers.build${
+      govHelperNetworkNames[network as Networks]
+    }(address(0));`;
+  })
+  .join('\n')}
+    GovHelpers.createProposal(payloads, '');
+  }
+}
+
+${Object.keys(updates)
+  .map((network) => {
+    return `contract Deploy${updateDate}Payload${network} is ${network}Script {
+  function run() external broadcast {
+    new AaveV3${network}Update${updateDate}Payload();
+  }
+}`;
+  })
+  .filter((x) => x)
+  .join('\n\n')}`;
+}
