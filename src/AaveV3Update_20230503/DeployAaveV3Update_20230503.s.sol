6351cfb71e1b864148ba0290b16cf50c1b970ef6
Jonathan Reem <jonathan.reem@gmail.com>
2023-05-03 13:36:20 -0700

    Add proposal description, add deployed addresses and ipfs hash.

--- src/AaveV3Update_20230503/DeployAaveV3Update_20230503.s.sol
+++ src/AaveV3Update_20230503/DeployAaveV3Update_20230503.s.sol
@@ -13,8 +13,8 @@ import {
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](1);
-    payloads[0] = GovHelpers.buildArbitrum(address(0));
-    GovHelpers.createProposal(payloads, '');
+    payloads[0] = GovHelpers.buildArbitrum(0x6bcE15B789e537f3abA3C60CB183F0E8737f05eC);
+    GovHelpers.createProposal(payloads, 0xd6c2cf5b04768c8ef63a635112fbc90cc57ec92bf5f70305c1a3f7769e8b4c51);
   }
 }
 
