355742b607ac88aff15375f9658f7f4a8cfe99b4
Jonathan Reem <jonathan.reem@gmail.com>
2023-08-04 14:37:14 -0700

    Add ipfs hash and deployed contract addresses.

--- src/AaveV3Update_20230804/DeployAaveV3Update_20230804.s.sol
+++ src/AaveV3Update_20230804/DeployAaveV3Update_20230804.s.sol
@@ -14,9 +14,9 @@ import {
 contract CreateProposal is EthereumScript {
   function run() external broadcast {
     GovHelpers.Payload[] memory payloads = new GovHelpers.Payload[](2);
-    payloads[0] = GovHelpers.buildMainnet(address(0));
-    payloads[1] = GovHelpers.buildPolygon(address(0));
-    GovHelpers.createProposal(payloads, '');
+    payloads[0] = GovHelpers.buildMainnet(0x7aa759A57c6b039A93e93683FacD14209EE9A3DD);
+    payloads[1] = GovHelpers.buildPolygon(0xc12aD8B3D242B1EDdc1C8319D1d58608E67043eD);
+    GovHelpers.createProposal(payloads, 0x4940eb2481c308072d4e2431c167477918e48edf25551c54bdac13d1f78d8497);
   }
 }
 
